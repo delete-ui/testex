7a5288fa2c4ec0a8cccd61aacc4652c0d7047319
delete-ui <papdzhon@yandex.ru>
2024-11-01 13:07:49 +0300

    Add files via upload

---
+++ test/test/test.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+double transform(double x) {
+    double a = -30.0; // нижняя граница нового диапазона
+    double b = 10.0;  // верхняя граница нового диапазона
+    return x * (b - a) + a;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    double input;
+
+    //имитация рандомного числа
+    std::cout << "Введите число от 0 до 1: ";
+    std::cin >> input;
+
+    double transformed = transform(input);
+    std::cout << "Преобразованное число: " << transformed << std::endl;
+
+    return 0;
+}
